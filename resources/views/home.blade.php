45e720b380a28b7d80e2f22ff8c4bc42e78d66d5
VladimirPiki <vlade.piki23@hotmail.com>
2023-04-09 21:41:30 +0200

    My first touch with laravel 10

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vladimir Piki</title>
+</head>
+<body>
+    <h1>Welcome to MACEDONIA !</h1>
+</body>
+</html>
